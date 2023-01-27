c1d78cb54f6582353af363cd83090637582f05a9
Maxym Zavadovych <zavadovich17@gmail.com>
2023-01-27 13:58:48 +0200

    initial commit

--- src/ui/pages/Hotels.js
+++ src/ui/pages/Hotels.js
@@ -7,37 +7,41 @@ import StarBorderIcon from '@mui/icons-material/StarBorder';
 import { selectorHotels } from '../../engine/core/hotels/selectors';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { Box, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 function Hotels(){
     const hotels = useSelector(selectorHotels.items);
     return (
         <Container sx={{width: 1200, display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
             {hotels.map((hotel) => (
-                <Card key={hotel.id} sx={{ width: '30%', minHeight: 200, border: '1px solid', borderColor: 'primary.main', margin: 1 }}>
-                    <CardContent sx={{position: 'relative'}}>
+                <Card key={hotel.id} sx={{ width: '30%', height: 190,  border: '1px solid', borderColor: 'primary.main', margin: 1, position: 'relative' }}>
+                    <CardContent sx={{}}>
                         
                         <Typography sx={{ fontSize: 22 }} component={'h1'}>
                         {hotel.name}
                         </Typography>
                         
                         
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        <Typography>
                         {hotel.address}
                         </Typography>
 
-                        <Box sx={{display: 'flex', justifyContent: 'space-between', }}>
+                        <Box sx={{position: 'absolute', bottom: 0, width: '100%'}}>
+                            
+                            <Box sx={{display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                <Typography variant="body2">
+                                {hotel.city}
+                                </Typography>
 
-                            <Typography variant="body2">
-                            {hotel.city}
-                            </Typography>
-
-                            <Typography sx={{display: 'flex', alignItems: 'center'}} variant="h5" component="h5">
-                            {hotel.hotel_rating} 
-                            <StarBorderIcon sx={{color: 'primary.main', fontSize: '40px'}}/>
-                            </Typography>
+                                <Typography sx={{display: 'flex', alignItems: 'center', }} variant="h5" component="h5">
+                                {hotel.hotel_rating} 
+                                <StarBorderIcon sx={{color: 'primary.main', fontSize: '30px'}}/>
+                                </Typography>
+                            </Box>
 
                         </Box>
+
                         
                     </CardContent>
                 </Card>
